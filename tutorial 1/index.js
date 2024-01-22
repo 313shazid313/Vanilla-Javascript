d498626ac163f3ddd56e31166b10af208617c8bc
“shazid <hossainshazid567@gmail.com>
2024-01-23 00:11:22 +0600

    first push

---
+++ tutorial 1/index.js
@@ -0,0 +1,27 @@
+var num = [123, 12, 123];
+console.log(typeof num); //object
+
+var tom = ["324", "234", "342"];
+console.log(typeof tom); //object
+
+var n1 = 132;
+console.log(typeof n1); //number
+
+var v2 = toString(n1);
+console.log(typeof v2); //converted to string
+
+var n2 = "312ihiuhiuh";
+console.log(typeof n2); //string
+
+var n3 = parseInt(n2);
+console.log(typeof n3); //converted to integer
+console.log(n3); //312
+
+var number = 12.12333;
+console.log(number.toFixed(2)); //doshomik er por kototuku nibo
+
+var p1 = 3.2311;
+console.log(p1.toPrecision(2)); //mott koto gula shongkha thakbe
+    
+
+console.log(Number('34234'))
